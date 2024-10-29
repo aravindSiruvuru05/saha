13dee22bcffe89107e38bcae959b4b627fbd525c
Aravind Siruvuru <siruvuruaravind@gmail.com>
2024-10-29 17:11:15 +0530

    updated mibile vs desktop view of service dashboard

--- web/components/pages/ride/RideHomeLayout.tsx
+++ web/components/pages/ride/RideHomeLayout.tsx
@@ -9,6 +9,7 @@ import { MyRides } from './MyRides';
 import { useIonRouter } from '@ionic/react';
 import { useActiveTab } from '../ActiveTabContext';
 import { ToggleBar } from '@/components/ui/commonComponents/ToggleBar';
+import { ServiceDashboardLayout } from '../ServiceDashboardLayout';
 
 const ActiveTabContext = createContext({
   activeTab: 'ride-search', // Default value
@@ -16,100 +17,54 @@ const ActiveTabContext = createContext({
 });
 
 export const RideHomeLayout = () => {
-  const query = new URLSearchParams(location.search);
-
-  // const [activeTab, setActiveTab] = useState('ride-search');
-  const { activeTab, setActiveTab } = useActiveTab();
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const router = useIonRouter(); // Use IonRouter for navigation
-
   return (
-    <div className="flex flex-col min-h-screen bg-gray-100">
-      <main
-        className="flex-grow min-h-screen flex items-start justify-center p-4 mb-20 overflow-auto bg-right md:bg-center bg-cover w-lg"
-        // style={{
-        //   backgroundImage: `url(${images.src})`,
-        // }}
-      >
-        {activeTab === 'publish' && (
-          <ToggleBar
-            options={[
-              {
-                label: 'Publish Ride',
-                component: <PublishRide />,
-              },
-              {
-                label: 'Publish Requests',
-                component: <>No implementation</>,
-              },
-            ]}
-          />
-        )}
-        {activeTab === 'ride-search' && (
-          <ToggleBar
-            options={[
-              {
-                label: 'Find Rides',
-                component: <RidesSearch />,
-              },
-              {
-                label: 'Find Requests',
-                component: <>No implementation</>,
-              },
-            ]}
-          />
-        )}
-        {activeTab === 'my-rides' && <MyRides />}
-      </main>
-
-      <footer className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 shadow-lg lg:pb-0">
-        <nav className="flex justify-around items-center h-16">
-          <button
-            onClick={() => router.push('/', 'root', 'replace')}
-            className={`flex flex-col items-center justify-center h-full bg-accent w-full ${
-              activeTab === 'ride-search'
-                ? 'text-primary font-bold'
-                : 'text-gray-500'
-            }`}
-          >
-            <Grip className="h-6 w-6" />
-          </button>
-          <button
-            onClick={() => setActiveTab('ride-search')}
-            className={`flex flex-col items-center justify-center w-full h-full  ${
-              activeTab === 'ride-search'
-                ? 'text-primary font-bold scale-110'
-                : 'text-gray-500'
-            }`}
-          >
-            <Search className="h-6 w-6" />
-            <span className="text-sm mt-1">Search</span>
-          </button>
-          <button
-            onClick={() => setActiveTab('publish')}
-            className={`flex flex-col items-center justify-center w-full h-full ${
-              activeTab === 'publish'
-                ? 'text-primary font-bold scale-110'
-                : 'text-gray-500'
-            }`}
-          >
-            <Plus className="h-6 w-6" />
-            <span className="text-sm mt-1">Publish</span>
-          </button>
-          <button
-            onClick={() => setActiveTab('my-rides')}
-            className={`flex flex-col items-center justify-center w-full h-full ${
-              activeTab === 'my-rides'
-                ? 'text-primary font-bold scale-110'
-                : 'text-gray-500'
-            }`}
-          >
-            <Route className="h-6 w-6" />
-            <span className="text-sm mt-1">My Rides</span>
-          </button>
-        </nav>
-      </footer>
-    </div>
+    <ServiceDashboardLayout
+      menuItems={[
+        {
+          id: 'ride-search',
+          label: 'Search',
+          component: (
+            <ToggleBar
+              options={[
+                {
+                  label: 'Find Rides',
+                  component: <RidesSearch />,
+                },
+                {
+                  label: 'Find Requests',
+                  component: <>No implementation</>,
+                },
+              ]}
+            />
+          ),
+          icon: <Search className="h-5 w-5" />,
+        },
+        {
+          id: 'publish',
+          label: 'Publish',
+          component: (
+            <ToggleBar
+              options={[
+                {
+                  label: 'Publish Ride',
+                  component: <PublishRide />,
+                },
+                {
+                  label: 'Publish Requests',
+                  component: <>No implementation</>,
+                },
+              ]}
+            />
+          ),
+          icon: <Plus />,
+        },
+        {
+          id: 'my-ride',
+          label: 'My Rides',
+          component: <MyRides />,
+          icon: <Route />,
+        },
+      ]}
+    />
   );
 };
